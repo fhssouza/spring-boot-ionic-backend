3eff30b65809b25c751b10d1587a7c0ddbc41f9e
fhssouza <fhssouza@gmail.com>
2023-01-06 14:45:29 -0300

    Estado e Cidade

---
+++ src/main/java/com/fhssouza/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.fhssouza.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fhssouza.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+}
