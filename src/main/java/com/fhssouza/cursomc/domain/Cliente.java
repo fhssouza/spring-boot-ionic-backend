5aa24f4838ab7bb621d590c68e9222b28b7b029e
fhssouza <fhssouza@gmail.com>
2023-01-17 18:35:06 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/fhssouza/cursomc/domain/Cliente.java
+++ src/main/java/com/fhssouza/cursomc/domain/Cliente.java
@@ -18,9 +18,9 @@ import javax.persistence.OneToMany;
 import com.fhssouza.cursomc.domain.enums.TipoCliente;
 
 @Entity
-public class Cliente implements Serializable{
+public class Cliente implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
@@ -35,6 +35,9 @@ public class Cliente implements Serializable{
 	@ElementCollection
 	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>();
+
+	@OneToMany(mappedBy = "cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
 
 	public Cliente() {
 	}
@@ -102,6 +105,14 @@ public class Cliente implements Serializable{
 
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
+	}
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
 	}
 
 	@Override
