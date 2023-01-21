68b0c610058d75a4d87e18b370873fdd93d48c35
fhssouza <fhssouza@gmail.com>
2023-01-21 16:38:20 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/fhssouza/cursomc/domain/Cliente.java
+++ src/main/java/com/fhssouza/cursomc/domain/Cliente.java
@@ -15,6 +15,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fhssouza.cursomc.domain.enums.TipoCliente;
 
 @Entity
@@ -36,6 +37,7 @@ public class Cliente implements Serializable {
 	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 
+	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 
