e8de27f4c763afce0eea095296097b59f378b7ec
fhssouza <fhssouza@gmail.com>
2023-01-30 15:55:48 -0300

    Validação sintatica com Bean Validation

--- src/main/java/com/fhssouza/cursomc/services/CategoriaService.java
+++ src/main/java/com/fhssouza/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.fhssouza.cursomc.domain.Categoria;
+import com.fhssouza.cursomc.dto.CategoriaDTO;
 import com.fhssouza.cursomc.repositories.CategoriaRepository;
 import com.fhssouza.cursomc.services.exceptions.DataIntegrityException;
 import com.fhssouza.cursomc.services.exceptions.ObjectNotFoundException;
@@ -54,5 +55,9 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), 
 				orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
