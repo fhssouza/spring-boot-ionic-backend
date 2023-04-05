9f1a1bf600b9dce958391c94ba0dbad380937cc7
Fábio Souza <fhssouza@gmail.com>
2023-04-05 18:13:19 -0300

    adicionando senha a cliente

--- src/main/java/com/fhssouza/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/fhssouza/cursomc/dto/ClienteNewDTO.java
@@ -24,7 +24,10 @@ public class ClienteNewDTO implements Serializable {
 	@NotEmpty(message = "Preenchimento obrigatório")
 	private String cpfOuCnpj;
 	
+	@NotEmpty(message = "Preenchimento obrigatório")
 	private Integer tipo;
+	
+	private String senha;
 	
 	@NotEmpty(message = "Preenchimento obrigatório")
 	private String logradouro;
@@ -152,6 +155,14 @@ public class ClienteNewDTO implements Serializable {
 
 	public void setCidadeId(Integer cidadeId) {
 		this.cidadeId = cidadeId;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
 	}
 	
 }
